2f66559ce1aaca3d2e893622aaf4ba31b06d8dff
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2023-04-10 11:45:08 +0545

    second commit

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 const jokes = document.querySelector("#joke");
 const jokeBtn = document.querySelector("#jokeBtn");
 
-const generateJokes = () => {
-  const setHeader = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-  fetch("https://icanhazdadjoke.com", setHeader)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      jokes.innerHTML = data.joke;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+// fetch
+// const generateJokes = () => {
+//   const setHeader = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//   fetch("https://icanhazdadjoke.com", setHeader)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       jokes.innerHTML = data.joke;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+// jokeBtn.addEventListener("click", generateJokes);
+// generateJokes();
+
+// Async await
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch("https://icanhazdadjoke.com", setHeader);
+    const data = await res.json();
+    jokes.innerHTML = data.joke;
+  } catch (error) {
+    console.log(`The error is ${err}`);
+  }
 };
 
 jokeBtn.addEventListener("click", generateJokes);
